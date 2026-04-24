8e91b8570388a7494afe8caf9081161266bb3821
Rakesh Sharma <rakeshsharma1712@gmail.com>
2026-04-24 12:24:54 +0530

    new changes to main

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_hi('New project for GIS')
+    print_hi('starting GIS data extraction')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
